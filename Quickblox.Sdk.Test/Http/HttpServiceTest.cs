8fb3b1191b57e64af26fd486d25f4db3640f2e9a
Anatoliy <anatoliy.krivchenko@injoit.com>
2015-04-10 13:07:26 +0300

    http service tests

---
+++ Quickblox.Sdk.Test/Http/HttpServiceTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+using Quickblox.Sdk.Core.Http;
+using Quickblox.Sdk.Core.Serializer;
+
+namespace Quickblox.Sdk.Test.Http
+{
+    //TODO: write more tests
+    [TestClass]
+    public class HttpServiceTest
+    {
+        [TestMethod]
+        public async Task GetTest()
+        {
+            var response = await HttpBase.GetAsync("http://google.com", null);
+        }
+
+        [TestMethod]
+        public async Task PostTest()
+        {
+            var response = await HttpBase.PostAsync("http://google.com", null, new NewtonsoftJsonSerializer(), new List<KeyValuePair<string,string>>());
+        }
+
+    }
+}
