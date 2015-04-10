b013757614022366277078cd2371e8679234f884
Anatoliy <anatoliy.krivchenko@injoit.com>
2015-04-10 19:15:02 +0300

    Private chat manager

--- Quickblox.Core/Modules/MessagesModule/PrivateChatManager.cs
+++ Quickblox.Core/Modules/MessagesModule/PrivateChatManager.cs
@@ -3,11 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using agsXMPP;
+using agsXMPP.protocol.client;
 
 namespace Quickblox.Sdk.Modules.MessagesModule
 {
     public class PrivateChatManager
     {
+        private XmppClientConnection xmpp;
+        private string otherUserJid;
+        private bool isInitialized;
+
         public event EventHandler OnMessageReceived;
 
         public event EventHandler OnLogin;
@@ -16,12 +22,28 @@ namespace Quickblox.Sdk.Modules.MessagesModule
 
         public PrivateChatManager(string userId, string password, string otherUserId, string appId, string chatEndpoint)
         {
-            
+            otherUserJid = string.Format("{0}-{1}@chat.quickblox.com ", otherUserId, appId);
+
+            XmppClientConnection xmpp = new XmppClientConnection(chatEndpoint);
+            xmpp.OnLogin += XmppOnOnLogin;
+            xmpp.Open(userId, password); 
         }
 
-        public void SendMessage(string message)
+        private void XmppOnOnLogin(object sender)
         {
+            isInitialized = true;
+            var handler = OnLogin;
+            if (handler != null)
+                handler(this, new EventArgs());
+        }
 
+        public bool SendMessage(string message)
+        {
+            if (!isInitialized)
+                return false;
+
+            xmpp.Send(new Message(otherUserJid, MessageType.chat, message));
+            return true;
         }
 
         public void Close()
