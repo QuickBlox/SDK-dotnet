2b4361fc07815342490685ad01bd8ed3c297ec16
Anatoliy <anatoliy.krivchenko@injoit.com>
2015-06-19 20:16:09 +0300

    Global search finished

---
+++ QMunicate_8.1/QMunicate.UI/QMunicate.Shared/Models/UserVm.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Windows.UI.Xaml.Media;
+using Quickblox.Sdk.Modules.UsersModule.Models;
+
+namespace QMunicate.Models
+{
+    public class UserVm
+    {
+        #region Ctor
+
+        public UserVm()
+        {
+        }
+
+        protected UserVm(User user)
+        {
+            FullName = user.FullName;
+        }
+
+        #endregion
+
+        #region Properties
+
+        public string FullName { get; set; }
+
+        public ImageSource Image { get; set; }
+
+        #endregion
+
+        #region Public methods
+
+        public static UserVm FromUser(User user)
+        {
+            return new UserVm(user);
+        }
+
+        #endregion
+
+    }
+}
--- QMunicate_8.1/QMunicate.UI/QMunicate.Shared/ViewModels/SearchViewModel.cs
+++ QMunicate_8.1/QMunicate.UI/QMunicate.Shared/ViewModels/SearchViewModel.cs
@@ -1,17 +1,71 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Net;
 using System.Text;
+using QMunicate.Models;
+using Quickblox.Sdk.Modules.UsersModule.Responses;
 
 namespace QMunicate.ViewModels
 {
     public class SearchViewModel : ViewModel
     {
+        #region Fields
+
         private string searchText;
+
+        #endregion
+
+        #region Ctor
+
+        public SearchViewModel()
+        {
+            GlobalResults = new ObservableCollection<UserVm>();
+        }
+
+        #endregion
+
+        #region Properties
 
         public string SearchText
         {
             get { return searchText; }
-            set { Set(ref searchText, value); }
+            set
+            {
+                if (Set(ref searchText, value))
+                    Search(searchText);
+            }
         }
+
+        public ObservableCollection<UserVm> GlobalResults { get; set; }
+
+        #endregion
+
+        #region Private methods
+
+        private async void Search(string searchQuery)
+        {
+            if (string.IsNullOrWhiteSpace(searchQuery)) return;
+
+            IsLoading = true;
+            var response = await QuickbloxClient.UsersClient.GetUserByFullNameAsync(searchQuery, null, null);
+            if (response.StatusCode == HttpStatusCode.OK)
+            {
+                GlobalResults.Clear();
+                foreach (UserResponse item in response.Result.Items)
+                {
+                    GlobalResults.Add(UserVm.FromUser(item.User));
+                }
+            }
+            else
+            {
+                GlobalResults.Clear();
+            }
+
+            IsLoading = false;
+        }
+
+        #endregion
+
     }
 }
