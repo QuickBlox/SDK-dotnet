7ad68d11635fb2556906616cd2f898786dc5c7bf
Anatoliy <anatoliy.krivchenko@injoit.com>
2015-07-17 13:22:40 +0300

    showing names for private chats

--- QMunicate_8.1/QMunicate.UI/QMunicate.Shared/Helper/DialogsManager.cs
+++ QMunicate_8.1/QMunicate.UI/QMunicate.Shared/Helper/DialogsManager.cs
@@ -9,10 +9,12 @@ using Windows.ApplicationModel.Core;
 using Windows.UI.Core;
 using QMunicate.Core.DependencyInjection;
 using QMunicate.Models;
+using Quickblox.Logger;
 using Quickblox.Sdk;
+using Quickblox.Sdk.Modules.ChatModule.Models;
 using Quickblox.Sdk.Modules.ChatModule.Requests;
-using Quickblox.Sdk.Modules.MessagesModule.Models;
 using Quickblox.Sdk.Modules.Models;
+using Message = Quickblox.Sdk.Modules.MessagesModule.Models.Message;
 
 namespace QMunicate.Helper
 {
@@ -55,7 +57,20 @@ namespace QMunicate.Helper
                 Dialogs.Clear();
                 foreach (var dialog in response.Result.Items)
                 {
-                    Dialogs.Add(DialogVm.FromDialog(dialog));
+                    //TODO: remove this when group chats are ready
+                    if (dialog.Type != DialogType.Private)
+                    {
+                        await FileLogger.Instance.Log(LogLevel.Debug, "Ignoring not private chat");
+                        continue;
+                    }
+
+                    var dialogVm = DialogVm.FromDialog(dialog);
+                    int otherUserId = dialogVm.OccupantIds.FirstOrDefault(o => o != quickbloxClient.CurrentUserId);
+                    var otherContact = quickbloxClient.MessagesClient.Contacts.FirstOrDefault(c => c.UserId == otherUserId);
+                    if(otherContact != null)
+                        dialogVm.Name = otherContact.Name;
+
+                    Dialogs.Add(dialogVm);
                 }
             }
         }
