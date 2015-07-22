e0228a8d6c95a6a9dcc8f02517d2b29ac11e64fc
Anatoliy <anatoliy.krivchenko@injoit.com>
2015-07-22 20:40:01 +0300

    Small fix

--- QMunicate_8.1/QMunicate.UI/QMunicate.Shared/App.xaml.cs
+++ QMunicate_8.1/QMunicate.UI/QMunicate.Shared/App.xaml.cs
@@ -201,7 +201,7 @@ namespace QMunicate
             dictionary.Add(ViewLocator.ForgotPassword, typeof(ForgotPasswordPage));
             dictionary.Add(ViewLocator.Dialogs, typeof(DialogsPage));
             dictionary.Add(ViewLocator.Chat, typeof(ChatPage));
-            dictionary.Add(ViewLocator.Settings, typeof(SettingsView));
+            dictionary.Add(ViewLocator.Settings, typeof(SettingsPage));
             dictionary.Add(ViewLocator.Search, typeof(SearchPage));
             dictionary.Add(ViewLocator.SendRequest, typeof(SendRequestPage));
             return new PageResolver(dictionary);
