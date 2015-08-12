9091ad705d75a21e18496c7f2c525acd54c6beac
Eduardo <kren1@inbox.ru>
2015-08-12 15:53:13 +0300

    Delete interface

--- Quickblox.Xamarin/Quickblox.Sdk/Modules/MessagesModule/Interfaces/IMessagesClient.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace Quickblox.Sdk
-{
-	public interface IMessagesClient
-	{
-	}
-}
-
--- Quickblox.Xamarin/Quickblox.Sdk/Modules/MessagesModule/MessageClient.cs
+++ Quickblox.Xamarin/Quickblox.Sdk/Modules/MessagesModule/MessageClient.cs
@@ -10,7 +10,7 @@ using System.IO;
 
 namespace Quickblox.Sdk.Modules.MessagesModule
 {
-    public class MessagesClient : IMessagesClient
+    public class MessagesClient
     {
         #region Fields
 
