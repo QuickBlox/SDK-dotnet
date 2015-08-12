c015b9d58aca593e4367826c772d24310e83648a
Eduardo <kren1@inbox.ru>
2015-08-12 11:46:01 +0300

    Add to xmpp

---
+++ Quickblox.Xamarin/Quickblox.Sdk/Modules/MessagesModule/Models/NotificationType.cs
@@ -0,0 +1,16 @@
+﻿namespace Quickblox.Sdk.Modules.MessagesModule.Models
+{
+    public enum NotificationType
+    {
+        None = 0,
+        FriendsRequest = 4,
+        FriendsAccept = 5,
+        FriendsReject = 6,
+        FriendsRemove = 7,
+        AddedDialog = 21,
+        NameDialog = 22,
+        PhotoDialog = 23,
+        LeaveDialog = 24,
+        CreateDialog = 25
+    }
+}
