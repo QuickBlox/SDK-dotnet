91da53e8781ed41e971ac4ab36369bfa053d7aad
Eduardo <kren1@inbox.ru>
2015-09-04 14:02:14 +0300

    Remove xamarin logger

---
+++ Quickblox.Xamarin/Quickblox.Sdk/FileLogger.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quickblox.Logger
+{
+    public class FileLogger : ILogger
+    {
+        #region Singleton
+
+        private static readonly FileLogger instance = new FileLogger();
+
+        private FileLogger()
+        {
+        }
+
+        public static FileLogger Instance
+        {
+            get { return instance; }
+        }
+
+        #endregion
+
+        #region Fields
+
+        private const string LogFileName = "Logs.txt";
+
+        #endregion
+
+        #region ILogger methods
+
+        public async Task Log(LogLevel logLevel, string message)
+        {
+            Debug.WriteLine("{0}: {1}", logLevel, message);
+        }
+
+        #endregion
+
+    }
+}
