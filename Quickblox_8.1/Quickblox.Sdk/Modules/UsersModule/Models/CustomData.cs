49658b8a0088fd8325f647347dd33f7f37d491ee
Anatoliy <anatoliy.krivchenko@injoit.com>
2016-01-06 16:57:42 +0200

    CustomData for User

---
+++ Quickblox_8.1/Quickblox.Sdk/Modules/UsersModule/Models/CustomData.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace Quickblox.Sdk.Modules.UsersModule.Models
+{
+    public class CustomData
+    {
+        [JsonProperty("avatar_url")]
+        public string AvatarUrl { get; set; }
+
+        [JsonProperty("is_import")]
+        public bool IsImport { get; set; }
+
+        [JsonProperty("status")]
+        public string Status { get; set; }
+    }
+}
