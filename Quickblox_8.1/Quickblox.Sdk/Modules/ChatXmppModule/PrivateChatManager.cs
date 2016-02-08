db893adbdfda7359494588dbd0f95815fc02d814
Anatoliy <anatoliy.krivchenko@injoit.com>
2016-02-08 15:26:35 +0200

    Refactoring. Sending notification messages is optional

--- Quickblox_8.1/Quickblox.Sdk/Modules/ChatXmppModule/PrivateChatManager.cs
+++ Quickblox_8.1/Quickblox.Sdk/Modules/ChatXmppModule/PrivateChatManager.cs
@@ -137,126 +137,80 @@ namespace Quickblox.Sdk.Modules.ChatXmppModule
         /// <summary>
         /// Adds other user to your roster, subsribes for his presence, and sends FriendRequest notification message.
         /// </summary>
-        /// <param name="friendName">Other user name in your roster</param>
+        /// <param name="createChatMessage">Notify an opponent with a chat message and add this message to the chat history.</param>
+        /// <param name="contactName">Opponents name in your contact list</param>
         /// <returns>Is operation successful</returns>
-        public bool AddToFriends(string friendName = null)
+        public bool AddToFriends(bool createChatMessage, string contactName = null)
         {
             var rosterManager = quickbloxClient.ChatXmppClient as IRosterManager;
-            if (rosterManager != null)
-            {
-                rosterManager.AddContact(new Contact() { Name = friendName ?? otherUserId.ToString(), UserId = otherUserId });
-            }
+            rosterManager?.AddContact(new Contact() { Name = contactName ?? otherUserId.ToString(), UserId = otherUserId });
 
             SubsribeForPresence();
 
-            var msg = CreateNewMessage();
-            var body = new body {Value = "Contact request"};
-            var extraParams = new ExtraParams();
-            extraParams.AddNew(ExtraParamsList.save_to_history, "1");
-            extraParams.AddNew(ExtraParamsList.dialog_id, dialogId);
-            extraParams.AddNew(ExtraParamsList.notification_type, ((int)NotificationTypes.FriendsRequest).ToString());
-
-            msg.Add(body, extraParams);
-            if (!xmppClient.Connected)
+            if (createChatMessage)
             {
-                xmppClient.Connect();
-                return false;
+                return SendFriendsNotification("Contact request", NotificationTypes.FriendsRequest);
             }
 
-            xmppClient.Send(msg);
             return true;
         }
 
         /// <summary>
         /// Adds other user to your roster, accepts presence subscription request, and sends FriendAccepted notification message.
         /// </summary>
-        /// <param name="friendName">Other user name in your roster</param>
+        /// <param name="createChatMessage">Notify an opponent with a chat message and add this message to the chat history.</param>
+        /// <param name="contactName">Opponents name in your contact list</param>
         /// <returns>Is operation successful</returns>
-        public bool AcceptFriend(string friendName = null)
+        public bool AcceptFriend(bool createChatMessage, string contactName = null)
         {
             var rosterManager = quickbloxClient.ChatXmppClient as IRosterManager;
-            if (rosterManager != null)
+            rosterManager?.AddContact(new Contact()
             {
-                rosterManager.AddContact(new Contact()
-                {
-                    Name = friendName ?? otherUserId.ToString(),
-                    UserId = otherUserId
-                });
-            }
+                Name = contactName ?? otherUserId.ToString(),
+                UserId = otherUserId
+            });
 
             ApproveSubscribtionRequest();
 
-            var msg = CreateNewMessage();
-            var body = new body {Value = "Request accepted"};
-            var extraParams = new ExtraParams();
-            extraParams.AddNew(ExtraParamsList.save_to_history, "1");
-            extraParams.AddNew(ExtraParamsList.dialog_id, dialogId);
-            extraParams.AddNew(ExtraParamsList.notification_type, ((int)NotificationTypes.FriendsAccept).ToString());
-
-            msg.Add(body, extraParams);
-            if (!xmppClient.Connected)
+            if (createChatMessage)
             {
-                xmppClient.Connect();
-                return false;
+                return SendFriendsNotification("Request accepted", NotificationTypes.FriendsAccept);
             }
 
-            xmppClient.Send(msg);
             return true;
         }
 
         /// <summary>
         /// Rejects subsription requests and sends FriendRejected notification message.
         /// </summary>
+        /// <param name="createChatMessage">Notify an opponent with a chat message and add this message to the chat history.</param>
         /// <returns>Is operation successful</returns>
-        public bool RejectFriend()
+        public bool RejectFriend(bool createChatMessage)
         {
             RejectSubscribtionRequest();
 
-            var msg = CreateNewMessage();
-            var body = new body {Value = "Request rejected"};
-            var extraParams = new ExtraParams();
-            extraParams.AddNew(ExtraParamsList.save_to_history, "1");
-            extraParams.AddNew(ExtraParamsList.dialog_id, dialogId);
-            extraParams.AddNew(ExtraParamsList.notification_type, ((int)NotificationTypes.FriendsReject).ToString());
-
-            msg.Add(body, extraParams);
-            if (!xmppClient.Connected)
+            if (createChatMessage)
             {
-                xmppClient.Connect();
-                return false;
+                return SendFriendsNotification("Request rejected", NotificationTypes.FriendsReject);
             }
 
-            xmppClient.Send(msg);
             return true;
         }
 
         /// <summary>
         /// Sends FriendRemoved notification messages, removes other user from your roster and unsubscribes from presence.
         /// </summary>
+        /// <param name="createChatMessage">Notify an opponent with a chat message and add this message to the chat history.</param>
         /// <returns>Is operation successful</returns>
-        public bool DeleteFromFriends()
+        public bool DeleteFromFriends(bool createChatMessage)
         {
-            var msg = CreateNewMessage();
-            var body = new body { Value = "Contact removed" };
-            var extraParams = new ExtraParams();
-            extraParams.AddNew(ExtraParamsList.save_to_history, "1");
-            extraParams.AddNew(ExtraParamsList.dialog_id, dialogId);
-            extraParams.AddNew(ExtraParamsList.notification_type, ((int)NotificationTypes.FriendsRemove).ToString());
-
-            msg.Add(body, extraParams);
-            if (!xmppClient.Connected)
+            if (createChatMessage)
             {
-                xmppClient.Connect();
-                return false;
+                return SendFriendsNotification("Contact removed", NotificationTypes.FriendsRemove);
             }
-
-            xmppClient.Send(msg);
 
             var rosterManager = quickbloxClient.ChatXmppClient as IRosterManager;
-            if (rosterManager != null)
-            {
-                rosterManager.DeleteContact(otherUserId);
-            }
+            rosterManager?.DeleteContact(otherUserId);
 
             Unsubscribe();
             SendPresenceInformation(presence.typeEnum.unsubscribed);
@@ -279,6 +233,27 @@ namespace Quickblox.Sdk.Modules.ChatXmppModule
                 id = MongoObjectIdGenerator.GetNewObjectIdString()
             };
         }
+
+        private bool SendFriendsNotification(string messageText, NotificationTypes notificationType)
+        {
+            var msg = CreateNewMessage();
+            var body = new body { Value = messageText };
+            var extraParams = new ExtraParams();
+            extraParams.AddNew(ExtraParamsList.save_to_history, "1");
+            extraParams.AddNew(ExtraParamsList.dialog_id, dialogId);
+            extraParams.AddNew(ExtraParamsList.notification_type, ((int)notificationType).ToString());
+
+            msg.Add(body, extraParams);
+            if (!xmppClient.Connected)
+            {
+                xmppClient.Connect();
+                return false;
+            }
+
+            xmppClient.Send(msg);
+            return true;
+        }
+
 
         private void MessagesClientOnOnMessageReceived(object sender, Message message1)
         {
