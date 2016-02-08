d9aa9b9eba8f14e76db51a7f5d2c3c3c38942688
Anatoliy <anatoliy.krivchenko@injoit.com>
2016-02-08 17:41:21 +0200

    Version 1.1.1

--- Quickblox_8.1/Quickblox.Sdk/Modules/ChatXmppModule/Models/AttachmentTag.cs
+++ Quickblox_8.1/Quickblox.Sdk/Modules/ChatXmppModule/Models/AttachmentTag.cs
@@ -7,26 +7,41 @@ using System.Xml.Linq;
 
 namespace Quickblox.Sdk.Modules.ChatXmppModule.Models
 {
+    /// <summary>
+    /// Information about attachment to be sent
+    /// </summary>
     public class AttachmentTag : Tag
     {
+        /// <summary>
+        /// UID 
+        /// </summary>
         public string Id
         {
             get { return (string)GetAttributeValue("id"); }
             set { SetAttributeValue("id", value); }
         }
 
+        /// <summary>
+        /// Attachment type
+        /// </summary>
         public string Type
         {
             get { return (string)GetAttributeValue("type"); }
             set { SetAttributeValue("type", value); }
         }
 
+        /// <summary>
+        /// Name
+        /// </summary>
         public string Name
         {
             get { return (string)GetAttributeValue("name"); }
             set { SetAttributeValue("name", value); }
         }
 
+        /// <summary>
+        /// URL
+        /// </summary>
         public string Url
         {
             get { return (string)GetAttributeValue("url"); }
